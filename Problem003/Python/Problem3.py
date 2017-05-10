b8b03526dc9fe00064c4564289302348fb629aaf
OmarHP <omar.hp90@gmail.com>
2017-05-10 14:09:02 -0500

    Add python solution for Problem 0m 3

---
+++ Problem003/Python/Problem3.py
@@ -0,0 +1,33 @@
+import math
+
+def getLargestPrimeFactor(n):
+
+	lastFactor = 1
+	factor = 2
+
+	while (n % factor == 0):
+		n = n // factor
+		lastFactor = factor
+
+	factor = 3
+	maxFactor = int( math.sqrt( n ) )
+	while ( n > 1 and factor < maxFactor ):
+		if(n % factor == 0):
+			n = n // factor
+			lastFactor = factor
+			while ( n % factor == 0 ):
+				n = n // factor
+			maxFactor = int( math.sqrt( n ) )
+		factor += 2
+	if n > 1:
+		return n
+
+	return lastFactor
+
+def main():
+	print(getLargestPrimeFactor(600851475143))
+	#print(getLargestPrimeFactor(13195))
+	#print(getLargestPrimeFactor(13195))
+
+if __name__ == "__main__":
+	main()
